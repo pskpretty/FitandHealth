c44a8f1bb2cb3a24f73dd23972e57d29e5e0e480
pskpretty <prettykurian@Prettys-MacBook-Air.local>
2017-05-08 09:17:29 +1000

    initial commit

---
+++ src/fitandHealth/Club.java
@@ -0,0 +1,44 @@
+package fitandHealth;
+
+import java.util.List;
+
+import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.query.space.grid.GridCell;
+import repast.simphony.query.space.grid.GridCellNgh;
+import repast.simphony.space.SpatialMath;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridPoint;
+
+public class Club {
+	private ContinuousSpace<Object> space;
+	private Grid<Object> grid;
+	private Boolean moved;
+	
+	public Club(ContinuousSpace<Object> space, Grid<Object> grid) {
+		super();
+		this.space = space;
+		this.grid = grid;
+	}
+	@ScheduledMethod(start = 1, interval = 1)
+	public void step(){
+		GridPoint pt = grid.getLocation(this);
+		GridCellNgh<PotentialAgents> nghCreator = new GridCellNgh<PotentialAgents>(grid, pt, PotentialAgents.class, 1, 1);
+		List<GridCell<PotentialAgents>> gridCells = nghCreator.getNeighborhood(true);
+		for (GridCell<PotentialAgents> cell : gridCells){
+		
+		}
+	}
+	public void moveTowards(GridPoint pt) {
+		if (!pt.equals(grid.getLocation(this))) {
+			NdPoint myPoint = space.getLocation(this);
+			NdPoint otherPoint = new NdPoint(pt.getX(), pt.getY());
+			double angle = SpatialMath.calcAngleFor2DMovement(space, myPoint, otherPoint);
+			space.moveByVector(this, 1, angle, 0);
+			myPoint = space.getLocation(this);
+			grid.moveTo(this, (int) myPoint.getX(), (int) myPoint.getY());
+			moved = true;
+		}
+	}
+}
---
+++ src/fitandHealth/ConsumerAgent.java
@@ -0,0 +1,18 @@
+package fitandHealth;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.grid.Grid;
+public class ConsumerAgent {
+	private ContinuousSpace<Object> space;
+	private Grid<Object> grid;
+	private Boolean moved;
+
+	public ConsumerAgent(ContinuousSpace<Object> space, Grid<Object> grid) {
+
+		this.space = space;
+		this.grid = grid;
+	}
+
+
+
+
+}
