60e6c59af75eb7d31c5ba4517b1a0bafc9bb542c
pretty <pskpretty@gmail.com>
2017-05-11 22:18:21 +1000

    base updation

--- src/fitandHealth/Club.java
+++ src/fitandHealth/Club.java
@@ -1,17 +1,15 @@
 package fitandHealth;
 
+import java.util.Iterator;
 import java.util.List;
 
 import repast.simphony.engine.schedule.ScheduledMethod;
 import repast.simphony.query.space.grid.GridCell;
 import repast.simphony.query.space.grid.GridCellNgh;
-import repast.simphony.random.RandomHelper;
-import repast.simphony.space.SpatialMath;
+import repast.simphony.query.space.grid.MooreQuery;
 import repast.simphony.space.continuous.ContinuousSpace;
-import repast.simphony.space.continuous.NdPoint;
 import repast.simphony.space.grid.Grid;
 import repast.simphony.space.grid.GridPoint;
-import repast.simphony.util.SimUtilities;
 
 public class Club {
 	private ContinuousSpace<Object> space;
@@ -29,20 +27,20 @@ public class Club {
 		GridPoint pt = grid.getLocation(this);
 		GridCellNgh<PotentialAgents> nghCreator = new GridCellNgh<PotentialAgents>(grid, pt, PotentialAgents.class, 1, 1);
 		List<GridCell<PotentialAgents>> gridCells = nghCreator.getNeighborhood(true);
-		SimUtilities.shuffle(gridCells, RandomHelper.getUniform());
-		for (GridCell<PotentialAgents> cell : gridCells){
+		MooreQuery<PotentialAgents> query=new MooreQuery(grid,this);
+		Iterator<PotentialAgents> iter=query.query().iterator();
+		for(Object o:query.query()){
+			System.out.println(o);
+			if(o instanceof PotentialAgents){
+				System.out.println("Potential agent"+o);
+			}
+		}
 		
+		broadcast = true;
+	}
+	public void inviteUpdation(GridPoint pt) {
+		
+			
 		}
 	}
-	public void invite(GridPoint pt) {
-		if (!pt.equals(grid.getLocation(this))) {
-			NdPoint myPoint = space.getLocation(this);
-			NdPoint otherPoint = new NdPoint(pt.getX(), pt.getY());
-			double angle = SpatialMath.calcAngleFor2DMovement(space, myPoint, otherPoint);
-			space.moveByVector(this, 1, angle, 0);
-			myPoint = space.getLocation(this);
-			grid.moveTo(this, (int) myPoint.getX(), (int) myPoint.getY());
-			broadcast = true;
-		}
-	}
-}
+
